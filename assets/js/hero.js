16902a31b7536d837bb4dfbad5abbfb79a20ea01
Daniel <danielallendeutsch@gmail.com>
2017-07-29 22:43:52 -0700

    Fix header, footer. Get carousel working with text. Minor fixups.

--- assets/js/hero.js
+++ assets/js/hero.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
   var i = 1;
+  var p = [ ["Endure", "Gracefully", "Mining Emergencies"],
+            ["Stay", "Afloat", "Marine Emergencies"],
+            ["Pull", "Through", "Terror Emergencies"] ]
 
-  setInterval(function(){ 
-    $("main").css("background", "url('../main/assets/images/hero" + i + ".jpg')");
+  setInterval(function(){
+    // $("main").css("background", "url('../main/assets/images/hero" + i + ".jpg')");
+    $("main").css("background", "url('../sos-2/assets/images/hero" + i + ".jpg')");
+    $("main span.one")[0].innerHTML = p[i][0];
+    $("main span.two")[0].innerHTML = p[i][1];
+    $("main span.three")[0].innerHTML = p[i][2];
     i < 2 ? i++ : i=0;
-  }, 5000);
+  }, 7500);
 });
