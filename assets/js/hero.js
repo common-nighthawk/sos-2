385dd5cabcb5728744b77ecefbe2da9552507944
Daniel <danielallendeutsch@gmail.com>
2017-08-30 20:53:57 -0700

    Prep for soft-launch (home and contact pages only)

--- assets/js/hero.js
+++ assets/js/hero.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
   var i = 1;
-  var p = [ ["Endure", "Gracefully", "Mining Emergencies"],
-            ["Stay", "Afloat", "Marine Emergencies"],
-            ["Pull", "Through", "Terror Emergencies"] ]
+  var p = [
+    ["Rise", "Above", "Aviation Emergencies"],
+    ["Stay", "Afloat", "Marine Emergencies"],
+    ["Pull", "Through", "Terror Emergencies"],
+    ["Endure", "Gracefully", "Mining Emergencies"],
+  ]
 
   setInterval(function(){
-    // $("main").css("background", "url('../main/assets/images/hero" + i + ".jpg')");
-    $("main").css("background", "url('../sos-2/assets/images/hero" + i + ".jpg')");
+    $("main").css("background", "url('../main/assets/images/hero" + i + ".jpg')");
+    // $("main").css("background", "url('../sos-2/assets/images/hero" + i + ".jpg')");
     $("main span.one")[0].innerHTML = p[i][0];
     $("main span.two")[0].innerHTML = p[i][1];
     $("main span.three")[0].innerHTML = p[i][2];
-    i < 2 ? i++ : i=0;
+    i < 3 ? i++ : i=0;
   }, 7500);
 });
