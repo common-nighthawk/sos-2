e0e0647469f27c4e66bd1087e37ba8aa1e25fd48
Daniel <danielallendeutsch@gmail.com>
2017-12-25 15:00:57 -0800

    Fixes to hero.

--- assets/js/hero.js
+++ assets/js/hero.js
@@ -8,11 +8,17 @@ $(document).ready(function() {
   ]
 
   setInterval(function(){
-    $("main").css("background", "url('../main/assets/images/hero" + i + ".jpg')");
+    // $("main").css("background", "url('../main/assets/images/hero" + i + ".jpg')");
     // $("main").css("background", "url('../sos-2/assets/images/hero" + i + ".jpg')");
+    $("main").css("background", "url('assets/images/hero" + i + ".jpg')");
     $("main span.one")[0].innerHTML = p[i][0];
     $("main span.two")[0].innerHTML = p[i][1];
     $("main span.three")[0].innerHTML = p[i][2];
+    if (i==2) {
+      $("main span.one, main span.two, main span.three").css("text-shadow", "-1px 0 #85919F, 0 1px #85919F, 1px 0 #85919F, 0 -1px #85919F");
+    } else {
+      $("main span.one, main span.two, main span.three").css("text-shadow", "none");
+    }
     i < 3 ? i++ : i=0;
   }, 7500);
 });
